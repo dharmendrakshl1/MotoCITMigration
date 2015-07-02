a6360d0c0cf8f8bf5c66f0e3d0f103a9de705ab8
dharmendrakshl1 <dharmendra.kshl1@gmail.com>
2015-07-02 16:09:08 +0530

    Update auth.js

--- config/auth.js
+++ config/auth.js
@@ -6,19 +6,19 @@ module.exports = {
     'facebookAuth' : {
         'clientID'        : '355143334507436', // your App ID
         'clientSecret'    : 'b589bf974c044f16e633ca665acaa1ea', // your App Secret
-        'callbackURL'     : 'http://ec2-52-27-39-13.us-west-2.compute.amazonaws.com:8080/auth/facebook/callback'
+        'callbackURL'     : 'http://uatlb-1950226449.us-west-2.elb.amazonaws.com/auth/facebook/callback'
     },
 
     'twitterAuth' : {
         'consumerKey'        : 'your-consumer-key-here',
         'consumerSecret'     : 'your-client-secret-here',
-        'callbackURL'        : 'http://localhost:8080/auth/twitter/callback'
+        'callbackURL'        : 'http://uatlb-1950226449.us-west-2.elb.amazonaws.com/auth/twitter/callback'
     },
 
     'googleAuth' : {
         'clientID'         : '9141745574-28o54bt7nrdeu44db424o6c1uaqstnjv.apps.googleusercontent.com',
         'clientSecret'     : 'rQRMQzgOi7mgod1qdREvVupS',
-        'callbackURL'      : 'http://ec2-52-27-39-13.us-west-2.compute.amazonaws.com:8080/auth/google/callback'
+        'callbackURL'      : 'http://uatlb-1950226449.us-west-2.elb.amazonaws.com/auth/google/callback'
     }
 
 };
