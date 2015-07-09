451212db9e134b2065fa586c4a0ace22ab4ebc06
csingh <csingh@csinghlt-01.dsa.mot-mobility.com>
2015-07-09 11:07:23 +0530

    Adding changes to app.js

--- app.js
+++ app.js
@@ -80,7 +80,8 @@ require('./config/passport')(passport); // pass passport for configuration
 
 // set up our express application
 app.use(morgan('dev')); // log every request to the console
-app.use(cookieParser()); // read cookies (needed for auth)
+app.use(cookieParser());
+app.set('views', __dirname + '/views');// read cookies (needed for auth)
 app.use(bodyParser.json()); // get information from html forms
 app.use(express.bodyParser());
 app.use(bodyParser.urlencoded({ extended: true }));
