206dcd92e1f71d4ccd1cc34e888ba6e85412a342
dharmendrakshl1 <dharmendra.kshl1@gmail.com>
2015-07-10 19:51:33 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -2,6 +2,7 @@
 module.exports = {
 
     //'url' : 'mongodb://10.30.6.153/passport' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
-	'url' : 'mongodb://dharmendra:dharm123@ds047632.mongolab.com:47632/dharmendra_test'
+    //'url' : 'mongodb://dharmendra:dharm123@ds047632.mongolab.com:47632/dharmendra_test'
+    'url' : 'mongodb://localhost/RSD_DEV1'
 
 };
