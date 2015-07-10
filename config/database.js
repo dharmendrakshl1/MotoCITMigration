58bc8c11fc945126c6758b7dfe8e5c34aac4c53e
csingh <csingh@csinghlt-01.dsa.mot-mobility.com>
2015-07-10 19:15:42 +0530

    Build4

--- config/database.js
+++ config/database.js
@@ -1,7 +1,9 @@
 // config/database.js
 module.exports = {
 
+	//local url
     'url' : 'mongodb://localhost/RSD_DEV1' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
-	//'url' : 'mongodb://dharmendra:dharm123@ds047632.mongolab.com:47632/dharmendra_test'
+	// this is url to connect to mongolab
+    
 
 };
