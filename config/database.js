3dae67e914949e8c2ae9214e5f69675b74b05bd0
dharmendrakshl1 <dharmendra.kshl1@gmail.com>
2015-07-13 13:10:16 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -2,8 +2,8 @@
 module.exports = {
 
 	//local url
-    'url' : 'mongodb://localhost/RSD_DEV1' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    //	'url' : 'mongodb://localhost/RSD_DEV1' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 	// this is url to connect to mongolab
-    
+    'url' : 'mongodb://dharmendra:dharm123@ds047632.mongolab.com:47632/dharmendra_test'
 
 };
