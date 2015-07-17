19e102e12561bff76b83a7b2d8381639faba860f
alourdu <alourdu@alourdult-01.dsa.mot-mobility.com>
2015-07-17 15:55:57 +0530

    Added time in JSON page

--- model/JsonFileManagement.js
+++ model/JsonFileManagement.js
@@ -5,6 +5,8 @@ var Schema = mongoose.Schema;
 
 //chethana 
 exports.jsonFileManagement = function (req, res) {
+	
+	var startTime = process.hrtime(); // start time to connect db
 	MongoClient.connect(configDB.url, function(err, db) {
 
 		if(err) { return console.dir(err); }
@@ -14,6 +16,13 @@ exports.jsonFileManagement = function (req, res) {
 		
 		console.log("Data selected from DB for drop down");
 		collection.find().toArray(function(err, items) {
+			var diffTime = process.hrtime(startTime); //  end time to connect to db
+			
+			console.log("While Database Connection JSONFILE : Time Taken in Seconds, NanoSeconds = "+(diffTime));
+			console.log("While Database Connection JSONFILE : Time Taken in Microseconds = "+(diffTime[0] * 1000000 + diffTime[1]/1000));
+			dbConntime = (diffTime); 
+			
+			
 		console.log("item in 1st select  : ",items);
 		var MarketName = [];
 		for(var ii = 0; ii<items.length; ii++)
@@ -22,8 +31,9 @@ exports.jsonFileManagement = function (req, res) {
 			MarketName.push(items[ii].MARKET_NAME);
 			}
 		var JsonDetail = [items[0].JSON_NAME,items[0].JSON_URL];
-		
-			res.render('\jsonfilemanagement ',{user: req.user,abc: {item1: items, item2: MarketName, item3: JsonDetail}});
+			
+		console.log("1 " + dbConntime);
+			res.render('\jsonfilemanagement ',{user: req.user,abc: {item1: items, item2: MarketName, item3: JsonDetail}, user1:dbConntime});
 		 });
 	
 		
