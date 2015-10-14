b6301bb48062a92b7c275edd64552a412b7dfc28
csingh <csingh@csinghlt-01.dsa.mot-mobility.com>
2015-10-14 12:08:40 +0530

    Build 09 without vpn

--- config/database.js
+++ config/database.js
@@ -1,10 +1,12 @@
 // config/database.js
 module.exports = {
 
+		 'url' : 'mongodb://csingh:Motorola123@ds037244.mongolab.com:37244/nodejspoc_db'
+			 
 	//local url
-		'url' : 'mongodb://10.30.6.160/RSD_DEV1'
+	//'url' : 'mongodb://10.30.6.160/RSD_DEV1'
     //'url' : 'mongodb://172.31.20.244/RSD_DEV1' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 	// this is url to connect to mongolab
      //'url' : 'mongodb://dharmendra:dharm123@ds047632.mongolab.com:47632/dharmendra_test'
-
+			//'url' : 'mongodb://ds047632.mongolab.com:47632/dharmendra_test'
 };
